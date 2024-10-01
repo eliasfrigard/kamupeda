16cf2f9f4c8b3a664d1f8f993b315ba32e7e93de
Elias Frigård <elias.frigard@tvtools.fi>
2024-10-01 21:55:21 +0300

    generate pages

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -6,26 +6,46 @@ const accessToken = process.env.ACCESS_TOKEN || ''
 
 type PageSkeleton = {
   contentTypeId: "page",
+  sys: {
+    id: string
+  }
   fields: {
     title: Contentful.EntryFieldTypes.Text
   }
 }
 
-export async function generateStaticParams() {
+const normalizeSlug = (title: string): string => {
+  return title.toLowerCase().replace(/\s+/g, '-')
+}
+
+const getPageData = async () => {
   const client = createClient({ space, accessToken })
 
   const pageRes = await client.getEntries<PageSkeleton>({
-    content_type: 'page',
+    content_type: 'page'
   })
 
-  const pages = pageRes.items
+  return pageRes.items
+}
+
+export async function generateStaticParams() {
+  const pages = await getPageData()
 
   return pages.map((page) => ({
-    slug: page.fields.title
+    slug: normalizeSlug(page.fields.title)
   }))
 }
 
 export default async function Page({ params }: { params: { slug: string } }) {
+  const normalizedSlug = params.slug
+
+  const pages = await getPageData()
+  const page = pages.find((p) => normalizeSlug(p.fields.title) === normalizedSlug)
+
+  if (!page) {
+    return <div>Page not found</div>
+  }
+
   return (
     <div className='w-full min-h-screen flex justify-center items-center text-4xl text-black'>{params.slug}</div>
   )
