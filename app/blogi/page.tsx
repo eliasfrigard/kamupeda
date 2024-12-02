7938aec00481ca88210b8134861c83db2e0a692f
Elias Frigård <elias.frigard@mediatailor.fi>
2024-12-02 12:06:45 +0200

    blog preview updates

--- app/blogi/page.tsx
+++ app/blogi/page.tsx
@@ -8,14 +8,24 @@ function truncateText(text: string, maxLength: number) {
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const mapBlogPost = (post: any) => {
+  console.log('🚀 || mapBlogPost || post:', post)
   return {
     sys: post.sys,
     fields: {
+      date: formatDate(post.sys.createdAt),
       title: post.fields.title,
       previewDescription: post.fields.previewDescription,
       previewImage: post.fields.previewImage,
     }
   }
+}
+
+const formatDate = (date: string) => {
+  return new Intl.DateTimeFormat('fi-FI', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  }).format(new Date(date))
 }
 
 export default async function Blog() {
@@ -26,19 +36,27 @@ export default async function Blog() {
     <div className="container mx-auto grid lg:grid-cols-3 gap-4 lg:gap-8 px-4">
       {
         mappedPosts.length && mappedPosts.map((post) => (
-          <Link key={post.sys.id} href={`/blogi/${post.sys.id}`} className="hover:scale-105 duration-200">
-            <div className="relative w-full aspect-square overflow-hidden rounded-xl shadow">
+          <Link key={post.sys.id} href={`/blogi/${post.sys.id}`} className="group hover:scale-[1.01] active:scale-100 duration-300 bg-primary-800 rounded-lg shadow-lg overflow-hidden">
+            <div className="relative w-full aspect-square overflow-hidden">
               <Image
                 alt={post?.fields?.previewImage?.fields?.file?.fileName} 
                 src={`http:${post?.fields?.previewImage?.fields?.file.url}`}
                 fill
-                className="object-cover"
+                className="object-cover group-hover:scale-105 transition-transform duration-300"
               />
             </div>
-            <div className="w-full flex flex-col p-4 text-black gap-3">
-              <h2 className="text-lg font-bold">{post.fields.title}</h2>
-              <div className="w-full h-[2px] bg-black bg-opacity-60 rounded-full" />
-              <p className="text-sm leading-relaxed opacity-80">
+            <div className="w-full flex flex-col p-6 text-white/80 gap-3 tracking-wide">
+              <h2 className="text-lg font-semibold">{post.fields.title}</h2>
+              <div className="flex items-center text-xs">
+                <p className="font-medium mr-2">
+                  Maija Karhinen-Ilo
+                </p>
+                (<p className="font-light">
+                  {post.fields.date}
+                </p>)
+              </div>
+              <div className="w-full h-[1px] bg-white/10 rounded-full" />
+              <p className="text-sm leading-relaxed opacity-80 text-pretty pr-2">
                 {truncateText(post.fields.previewDescription, 150)} {/* Adjust character limit as needed */}
               </p>
             </div>
