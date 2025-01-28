a14d24831e6d160bdbb754f35ad9472137996f62
Elias Frigård <elias.frigard@mediatailor.fi>
2025-01-28 22:50:18 +0200

    remove console log

--- components/TextLayout.js
+++ components/TextLayout.js
@@ -35,7 +35,6 @@ const options = {
     },
     [BLOCKS.EMBEDDED_ASSET]: (node) => {
       const { url, fileName, contentType } = node.data.target.fields.file;
-      console.log("🚀 || url:", url);
 
       if (fileName.includes("mxl") || fileName.includes("xml")) {
         return (
