df2383ae8902b06dbd626ab71d06d0dc5cb9453d
Elias Frigård <elias.frigard@mediatailor.fi>
2025-02-11 20:15:34 +0200

    sticky state with use effect

--- hooks/useStickyState.tsx
+++ hooks/useStickyState.tsx
@@ -1,15 +1,28 @@
 import { useEffect, useState } from "react";
 
 const useStickyState = (defaultValue, key) => {
-  const [value, setValue] = useState(() => {
-    const stickyValue = window.localStorage.getItem(key);
+  const [value, setValue] = useState(defaultValue);
+  const [isClient, setIsClient] = useState(false);
 
-    return stickyValue !== null ? JSON.parse(stickyValue) : defaultValue;
-  });
+  // This ensures the code runs only after the component mounts in the client-side environment
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
 
   useEffect(() => {
-    window.localStorage.setItem(key, JSON.stringify(value));
-  }, [key, value]);
+    if (isClient) {
+      const stickyValue = window.localStorage.getItem(key);
+      if (stickyValue !== null) {
+        setValue(JSON.parse(stickyValue));
+      }
+    }
+  }, [isClient, key]);
+
+  useEffect(() => {
+    if (isClient) {
+      window.localStorage.setItem(key, JSON.stringify(value));
+    }
+  }, [isClient, value, key]);
 
   return [value, setValue];
 };
