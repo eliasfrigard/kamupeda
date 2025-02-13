1c6327409f356f07bedd315ea10e0a9e08461a5a
Elias Frigård <elias.frigard@mediatailor.fi>
2025-02-13 20:25:53 +0200

    category section counts

--- components/Material/CategorySection.tsx
+++ components/Material/CategorySection.tsx
@@ -18,15 +18,21 @@ type InstrumentSectionProps = {
 };
 
 const InstrumentSection = ({ category, materials }: InstrumentSectionProps) => {
+  console.log("🚀 || InstrumentSection || materials:", materials.length);
   return (
     <Disclosure defaultOpen={false}>
       {({ open }) => (
         <>
           <DisclosureButton className='flex flex-col'>
             <div className='w-full flex items-center justify-between py-4 px-2'>
-              <h3 className='text-2xl text-primary-700 font-semibold tracking-wide'>
-                {category}
-              </h3>
+              <div className='flex justify-center items-center gap-3'>
+                <h3 className='text-2xl text-primary-700 font-semibold tracking-wide'>
+                  {category}
+                </h3>
+                <div className='h-6 aspect-square shadow rounded-full text-sm flex justify-center items-center bg-gradient-to-r from-accent-500 to-accent-500'>
+                  <p>{materials.length}</p>
+                </div>
+              </div>
               <FaChevronUp
                 className={`text-2xl text-primary-700 transition-transform duration-300 ease-in-out transform ${
                   !open ? "rotate-180" : ""
