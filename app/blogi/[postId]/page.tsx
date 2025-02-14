6f109f9e98ec22f2a7a5e5b2b91f5b64ec5ea49d
Elias Frigård <elias.frigard@mediatailor.fi>
2025-02-14 12:12:17 +0200

    move blog page info

--- app/blogi/[postId]/page.tsx
+++ app/blogi/[postId]/page.tsx
@@ -2,6 +2,8 @@ import { normalizeSlug } from "@/utils/normalizeSlug";
 import { getBlogPostData, getPostById } from "@/utils/contentful";
 
 import type { BlogPostSkeleton } from "@/types";
+import Divider from "@/components/Divider";
+import { dancingScript } from "@/app/fonts";
 
 import TextLayout from "@/components/TextLayout";
 
@@ -11,15 +13,6 @@ export const generateStaticParams = async () => {
   return blogPosts?.map((post) => ({
     postId: normalizeSlug(post.sys.id),
   }));
-};
-
-const InfoItem = ({ label, value }) => {
-  return (
-    <div className='flex flex-col gap-2 items-start'>
-      <p className='text-sm font-medium text-primary-900/70'>{label}:</p>
-      <p className='font-medium'>{value}</p>
-    </div>
-  );
 };
 
 const formatDate = (date: string) => {
@@ -51,19 +44,35 @@ export default async function Page({ params }: { params: { postId: string } }) {
   return (
     <div className='w-screen'>
       <div className='container mx-auto text-black flex flex-col justify-center items-center gap-10 px-6 lg:px-4 max-w-4xl'>
-        <TextLayout className='' text={page.fields.textContent} />
-
-        <div className='bg-gradient-to-r from-accent-500/30 to-primary-800/30 w-full h-[1px] rounded-full' />
-
-        <div className='flex flex-col gap-4 w-full justify-start items-start'>
-          <InfoItem label='Otsikko' value={page.fields.title} />
-          <InfoItem label='Julkaistu' value={formattedCreatedAt} />
-          <InfoItem label='Viimeksi Päivitetty' value={formattedUpdatedAt} />
-          <InfoItem
-            label='Kirjoittaja'
-            value={authors?.map((author) => author.fields.name).join(", ")}
-          />
+        <div className='w-full flex flex-col gap-6'>
+          <h1 className={`text-5xl font-semibold ${dancingScript.className}`}>
+            {page.fields.title}
+          </h1>
+          <Divider className='opacity-20' />
+          <div className='flex flex-wrap gap-5'>
+            {authors &&
+              authors?.map((author) => (
+                <p
+                  className='text-accent-500 underline font-medium'
+                  key={author.sys.id}
+                >
+                  {author.fields.name}
+                </p>
+              ))}
+            <div className='flex gap-5 items-end flex-wrap'>
+              <div className='flex gap-1 justify-end items-center'>
+                <p className='opacity-60 text-xs -mb-[2px]'>Julkaistu:</p>
+                <p className='font-medium text-sm'>{formattedCreatedAt}</p>
+              </div>
+              <div className='flex gap-1 justify-end items-center'>
+                <p className='opacity-60 text-xs -mb-[2px]'>Päivitetty:</p>
+                <p className='font-medium text-sm'>{formattedUpdatedAt}</p>
+              </div>
+            </div>
+          </div>
         </div>
+
+        <TextLayout className='' text={page.fields.textContent} />
       </div>
     </div>
   );
--- components/Divider.tsx
+++ components/Divider.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-const Divider: React.FC = ({ className = "" }: { className: string }) => {
+const Divider = ({ className = "opacity-80" }: { className?: string }) => {
   return (
     <div
-      className={`h-[1px] bg-gradient-to-r from-accent-800 to-primary-800 opacity-80 rounded-full w-full ${className}`}
+      className={`h-[1px] bg-gradient-to-r from-accent-800 to-primary-800 rounded-full w-full ${className}`}
     />
   );
 };
