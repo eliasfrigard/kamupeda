b774fa895ed28cb688a22e003d016b10c32441c3
Elias Frigård <elias.frigard@mediatailor.fi>
2025-02-16 11:59:40 +0200

    fix search page with suspense

--- app/materiaalit/haku/page.tsx
+++ app/materiaalit/haku/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import Search from "@/components/Search";
 
-export default function Blog() {
-  return <Search />;
+export default function Haku() {
+  return (
+    <Suspense fallback={<div>Loading search...</div>}>
+      <Search />
+    </Suspense>
+  );
 }
